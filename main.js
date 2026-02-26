f17cbd5b75d1c7de741089f470ad38ced903b635
Aqdas-Abbasi-06 <2502114@students.au.edu.pk>
2026-02-26 09:49:43 +0500

    lab tasks
    done my lab tasks

---
+++ main.js
@@ -0,0 +1,6 @@
+import{addition,subtraction}from './math-utils.js';
+const sum=addition(10,5);
+const subt=subtraction(20,7);
+
+console.log("10+5",sum);
+console.log("20-7",subt);
---
+++ math-utils.js
@@ -0,0 +1,7 @@
+export const addition=(a,b)=>{
+    return(a+b);
+}
+export const subtraction=(c,d)=>{
+    return(c-d);
+
+}
